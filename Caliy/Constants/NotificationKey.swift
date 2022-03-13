d767e4648537826dd21d8382a501277ef96b5e4e
hanmok <dmammmm@naver.com>
2022-03-13 14:19:03 +0900

    Fix CalcApp to work

---
+++ Caliy/Constants/NotificationKey.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationKey2.swift
+//  Caliy
+//
+//  Created by Mac mini on 2022/03/13.
+//  Copyright © 2022 Mac mini. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationKey : String{
+    case ansFromTableNotification
+    case processToBaseVCNotification
+    case resultToBaseVCNotification
+    case sendingToastNotification
+    case sendUpdatingUserDefaultNotification
+    case viewWilltransitionNotification
+    case viewWillAppearbaseViewController
+    case viewWillDisappearbaseViewController
+    case resultTextColorChangeNotification
+    case progressViewScrollToVisibleNotification
+    case answerToTableNotification
+    
+}
